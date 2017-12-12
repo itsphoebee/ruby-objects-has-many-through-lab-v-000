2a5349b433b04821840ca3c097cbad8b06533653
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 02:16:44 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,24 +1,24 @@
 class Genre
 attr_accessor :name
 
-def initialize(name)
-  @name = name
-  @songs = []
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def songs
-  @songs
-end
+  def songs
+    @songs
+  end
 
-def artists
-  artists = []
-  self.songs.map {|song| artists << song.artist}
-  artists
-end
+  def artists
+    artists = []
+    self.songs.map {|song| artists << song.artist}
+    artists
+  end
 
-def add_song(song)
-  self.songs << song
-  song.genre = self
-end
+  def add_song(song)
+    self.songs << song
+    song.genre = self
+  end
 
 end
