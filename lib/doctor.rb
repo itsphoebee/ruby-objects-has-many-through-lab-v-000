0346a6bfc14a053f018bb75623c155541502bddb
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 01:53:15 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,23 +1,4 @@
 class Doctor
-attr_accessor :name
 
-def initialize(name)
-  @name = name
-  @appointments = []
-end
-
-def add_appointment(date)
-  @appointments << date
-  date.doctor = self
-end
-
-def appointments
-  @appointments
-end
-
-def patients
-  patients = []
-  @appointments.map {|appt| patients << appt.patient}
-end
 
 end
