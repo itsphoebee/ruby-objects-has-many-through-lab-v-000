88b6cedbfb223fc90ed1745f5222173ea93bb003
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 02:13:41 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,13 +6,13 @@ attr_accessor :name
     @appointments
   end
 
+  def appointments
+    @appointments
+  end
+  
   def add_appointment(appointment)
     self.appointments << appointment
     appointment.patient = self
-  end
-
-  def appointments
-    @appointments
   end
 
   def doctors
